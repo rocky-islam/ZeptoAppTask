9b6da4eae1a0e066f64362f603b7e0a5fc3935e9
Rocky islam <mohammadrockyislam01@gmail.com>
2024-10-17 23:17:14 +0600

    all details page done

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -4,16 +4,18 @@ const BookDetails = () => {
   const bookDetails = useLoaderData();
   console.log(bookDetails.id);
 
-  const { title, formats, authors, subjects, bookshelves } = bookDetails;
+  const { title, formats, authors, subjects, bookshelves, download_count } = bookDetails;
 
   return (
     <div>
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
+          <div className="flex w-2/4 p-4">
           <img
             src={`${formats['image/jpeg']}`}
-            className=" rounded-lg shadow-2xl"
+            className=" rounded-lg shadow-2xl w-full"
           />
+          </div>
           <div>
             <h1 className="text-5xl font-bold">{title}</h1>
             <p className="py-6">
@@ -24,6 +26,12 @@ const BookDetails = () => {
             </p>
             <p>
                 <strong>Bookshelves:</strong> {bookshelves[0]}
+            </p>
+            <p>
+                <strong>Languages:</strong> English
+            </p>
+            <p>
+                <strong>Total Download:</strong> {download_count}
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
